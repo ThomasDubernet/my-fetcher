cb2d74cc7678b7bfa5ebe6625ff7b22c0d6c1076
ThomasDubernet <thoms.dubernet@gmail.com>
2023-04-14 10:57:37 +0200

    first commit

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+interface User {
+    username: string
+    password: string
+    email: string
+    age: number
+    active: boolean
+}
+
+const user: User = {
+    username: 'John',
+    password: '123456',
+    email: 'test',
+    age: 25,
+    active: true,
+}
